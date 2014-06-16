b52d4cc816d92388c2772f7c524308c14f1dafee
Alex Kozlov <alexssource@gmail.com>
2014-06-17 00:23:45 +0300

    init

---
+++ src/main/java/com/daoleen/devicemeeting/web/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.daoleen.devicemeeting.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by alex on 16.6.14.
+ */
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String index() {
+        return "home/index";
+    }
+
+}
